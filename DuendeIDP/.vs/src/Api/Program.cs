e9c7579b24affeaea2a35e808983130d2479f315
Sabariah <sabariahsaifulrahim5146@gmail.com>
2023-01-25 11:56:18 +0800

    initial

---
+++ DuendeIDP/.vs/src/Api/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod());
+});
+
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.Audience = "https://localhost:5001/resources";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "api1");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CorsPolicy");
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers().RequireAuthorization();
+
+
+app.Run();
---
+++ DuendeIDP/.vs/src/IdentityServer/Config.cs
@@ -0,0 +1,60 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+    new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+    };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+    new List<ApiScope>
+    {
+        new ApiScope(name: "api1", displayName: "MyAPI")
+    };
+
+    public static IEnumerable<Client> Clients =>
+    new List<Client>
+    {
+        // interactive ASP.NET Core Web App
+        new Client
+        {
+            ClientName = "Angular-Client",
+            ClientId = "Client1",
+            ClientSecrets = { new Secret("SuperSecretPassword".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+
+
+            AllowedCorsOrigins = { "http://localhost:4200" },
+
+            AllowAccessTokensViaBrowser = true,
+
+            RequirePkce = true,
+
+            
+            // where to redirect to after login
+            RedirectUris = { "http://localhost:4200/auth-callback" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "http://localhost:4200/logout-callback" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                "api1"
+            },
+
+            RequireConsent = false,
+            AccessTokenLifetime = 600
+
+        }
+    };
+
+}
