b47bb99719d7ce58762efb8ebcaf16fe4ce4a6f6
Sabariah <sabariahsaifulrahim5146@gmail.com>
2023-01-25 12:02:08 +0800

    client

---
+++ ClientAngular/src/app/logout-callback/logout-callback.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-logout-callback',
+  templateUrl: './logout-callback.component.html',
+  styleUrls: ['./logout-callback.component.css']
+})
+export class LogoutCallbackComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.completeSignOut();
+  }
+
+}
---
+++ ClientAngular/src/app/redirect-server/redirect-server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redirect-server',
+  templateUrl: './redirect-server.component.html',
+  styleUrls: ['./redirect-server.component.css']
+})
+export class RedirectServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
