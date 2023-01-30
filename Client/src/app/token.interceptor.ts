f43f18c67fabdd8201188b77f18678b5120f387f
Sabariah <sabariahsaifulrahim5146@gmail.com>
2023-01-30 09:49:58 +0800

    client angular

---
+++ Client/src/app/token.interceptor.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { from, Observable, throwError } from 'rxjs';
+import { catchError, switchMap, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private authService:AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
+
+    var token = sessionStorage.getItem('token');
+    // token invalid
+    if (token && this.authService.tokenExpired(token)) {
+      console.log("token invalid")
+      return next.handle(request).pipe(
+        catchError(error =>{
+          if(error.status === 401){
+            console.log('token has expired')
+            return from(this.authService.refreshToken()).pipe(
+              switchMap(token => {
+                sessionStorage.removeItem('token');
+                sessionStorage.setItem('token', token);
+                request = request.clone({
+                  setHeaders:{
+                    Authorization: `Bearer ${token}`
+                  }
+                });
+                return next.handle(request);
+              })
+            )
+          }
+          else{
+            return throwError(error);
+          }
+        })
+      );
+    }
+    //token valid
+    else{
+      console.log("token valid")
+      request = request.clone({
+        setHeaders:{
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next.handle(request)
+    }
+  }
+  
+  
+
+
+    
+
+    //const clonedRequest = request.clone({ headers: request.headers.set('Authorization', `Bearer ` + token ) });
+  //   return next.handle(clonedRequest).pipe(
+  //     catchError(error => {
+  //       if (error.status === 401) {
+  //         console.log("token has expired")
+  //         return from(this.authService.refreshToken()).pipe(
+  //           switchMap(token=>{
+  //             request=request.clone({
+  //               setHeaders:{
+  //                 Authorization:`Bearer ${token}`
+  //               }
+  //             });
+  //             console.log("1")
+  //             return next.handle(request);
+  //           })
+  //         )}
+  //       else{
+  //         return throwError(error);
+  //       }
+  //   }),
+  //   )
+
